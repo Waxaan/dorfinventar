ac5cb58b28013a5f840ad29fae359538b66a810b
Jannik Meinecke <jannikmeinecke@web.de>
2020-01-25 19:22:35 +0100

    another quick fix (because the last one didnt work)

--- backend/routes.py
+++ backend/routes.py
@@ -82,7 +82,7 @@ def get_categories():
 def get_articles():
     print("Getting articles")
     query = Article.query
-    if 'owner' in request.args and current_identity is not None:
+    if 'owner' in request.args and current_identity != None:
         query = query.filter(Article.owner == current_identity.username)
     if 'name' in request.args:
         query = query.filter(Article.name.ilike("%"+request.args['name']+"%"))
